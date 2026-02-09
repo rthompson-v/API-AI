460ce71f36a1c6796391fe89eef372a01f92560b
rthompson-v <rthompson@everscalegroup.com>
2026-02-09 11:26:53 -0600

    Correcion
    Se añadio la ruta para llamar a la funcion en el backedn

--- routes/candidates.js
+++ routes/candidates.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { profileView, candidatesSearch } from "../controllers/candidatesController.js";
+import { profileView, candidatesSearch, profileViewByRole } from "../controllers/candidatesController.js";
 
 const router = express.Router();
 
 router.get("/profile-view", profileView);
 router.post("/search", candidatesSearch);
+router.post("/profile-view-by-role", profileViewByRole);
 
 export default router;
