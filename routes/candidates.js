e247ce5f823f57792a09588306384210bec7d366
rthompson-v <rthompson@everscalegroup.com>
2026-02-20 13:12:35 -0600

    Correcion
    Se eliminaron rutas inexsistentes y/o borradas

--- routes/candidates.js
+++ routes/candidates.js
@@ -7,24 +7,25 @@ import { profileView, candidatesSearch, profileViewByRole, addRecruiterManager, 
 const router = express.Router();
 
 
-//Funciones de Permisos
 
-router.get("/", requireAuth, getCandidates);
-router.post("/", requireAuth, requireRoleIds([1, 2]), createCandidate);
 
 //Funciones de Datos
 
-
+//Client Portal
 router.get("/profile-view", profileView);
-
-router.get("/catalogs/locations", getLocations);
-router.get("/catalogs/roles", getRoles);
-router.get("/catalogs/technologies", getTechnologies);
-router.get("/catalogs/technologies/:technology_id/modules", getModulesByTechnology);
-router.get("/catalogs/modules/:module_id/submodules", getSubmodulesByModule);
-router.post("/profile-view-by-role", profileViewByRole);
-router.post("/add-recruiter-manager", addRecruiterManager);
 router.post("/search", candidatesSearch);
-router.put("/update/:candidate_code", updateCandidateByCode);
+
+
+//User Talent System
+router.get("/catalogs/locations",requireAuth, getLocations);
+router.get("/catalogs/roles", requireAuth, getRoles);
+router.get("/catalogs/technologies", requireAuth, getTechnologies);
+router.get("/catalogs/technologies/:technology_id/modules", requireAuth, getModulesByTechnology);
+router.get("/catalogs/modules/:module_id/submodules", requireAuth, getSubmodulesByModule);
+router.post("/profile-view-by-role", requireAuth, requireRoleIds([1, 2]), profileViewByRole);
+router.post("/add-recruiter-manager", requireAuth, requireRoleIds([1, 2]), addRecruiterManager);
+router.put("/update/:candidate_code", requireAuth, requireRoleIds([1, 2]),   updateCandidateByCode);
+
+
 
 export default router;
